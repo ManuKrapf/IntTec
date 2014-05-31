d2b18c3735ed98e9e4db716b92d42ca116b77cd5
JLengdobler <johannes.lengdobler@stud.uni-regensburg.de>
2014-05-31 16:53:54 +0200

    added wiimote.py, worked on level.py

---
+++ blatt5/level.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import wiimote
+from PyQt4 import QtGui, QtCore
+
+class BubbleLevel():
+ 
+    def __init__(self, example_text):
+     
+        
+    
+def main():
+
+
+if __name__ == '__main__':
+    main()
