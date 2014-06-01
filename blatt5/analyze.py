462c420c98399ff3a7928d635642451ef852c38c
ManuKrapf <manuel@krapfnet.de>
2014-06-01 23:40:44 +0200

    task 2 didn't  workand can't be fixed

--- blatt5/analyze.py
+++ blatt5/analyze.py
@@ -2,43 +2,46 @@
 # -*- coding: utf-8 -*-
 
 import wiimote
+import sys
 import pyqtgraph as pg
 from pyqtgraph.flowchart import Flowchart
 from PyQt4 import QtGui, QtCore
 
-class Analyze():
- 
+
+class Analyze(QtGui.QWidget):
+
     def __init__(self, mac_address):
-	wm = wiimote.connect(mac_address)
-	self.initFlochart()
-	self.getData() # get accelerometer data to plot
-	self.plotData()
+        super(Analyze, self).__init__()
+        self.wm = wiimote.connect(mac_address)
+        self.initFlowchart()
+        self.getData() # get accelerometer data to plot
+        self.plotData()
 
     def initFlowchart(self):
-	fc = Flowchart(terminals={
-    	    'nameOfInputTerminal': {'io': 'in'},
-    	    'nameOfOutputTerminal': {'io': 'out'}
-	})
-	ctrl = fc.ctrlWidget()
-	myLayout.addWidget(ctrl)
+        fc = Flowchart(terminals={
+            'nameOfInputTerminal': {'io': 'in'},
+            'nameOfOutputTerminal': {'io': 'out'}
+        })
+        ctrl = fc.ctrlWidget()
+        #self.addWidget(ctrl)
 
-	fc.setInput(""" accelerometer data """)
-	output = fc.output()
-	output = fc.process(nameOfInputTerminal=newValue)
+        fc.setInput(self.wm.accelerometer)
+        output = fc.output()
+        output = fc.process(nameOfInputTerminal="newValue")
 
     def getData(self):
-	
-    
-    def plotData(self, data1, data2, data3):
-	win = pg.GraphicsWindow()  # Automatically generates grids with multiple items
-	win.addPlot(data1, row=0, col=0)
-	win.addPlot(data2, row=0, col=1)
-	win.addPlot(data3, row=1, col=0, colspan=2)
+        self.data = self.wm.accelerometer
 
-pg.show(imageData)  # imageData must be a numpy array with 2 to 4 dimensions
-    
+    def plotData(self, data1, data2, data3):
+        win = pg.GraphicsWindow()  # Automatically generates grids with multiple items
+        win.addPlot(self.data[0], row=0, col=0)
+        win.addPlot(self.data[1], row=0, col=1)
+        win.addPlot(self.data[2], row=1, col=0, colspan=2)
+        pg.show(self.data)  # imageData must be a numpy array with 2 to 4 dimensions
+
 def main():
     app = QtGui.QApplication(sys.argv)
+    analyze = Analyze(sys.argv[1])
     sys.exit(app.exec_())
 
 if __name__ == '__main__':
