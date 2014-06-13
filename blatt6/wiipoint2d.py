9cda2fd00e0528c7570ecde5ff9f868a89147b0d
ManuKrapf <manuel@krapfnet.de>
2014-06-13 16:29:35 +0200

    update

--- blatt6/wiipoint2d.py
+++ blatt6/wiipoint2d.py
@@ -2,64 +2,193 @@
 # coding: utf-8
 # -*- coding: utf-8 -*-
 
+import sys
 import wiimote
-import wiimote_node as wnode
 from pyqtgraph.flowchart import Flowchart, Node
+from pyqtgraph.flowchart.library.common import CtrlNode
 import pyqtgraph.flowchart.library as fclib
 from pyqtgraph import QtGui, QtCore
 import pyqtgraph as pg
+import numpy as np
 
-
-class Pointer2d(wnode.WiimoteNode):
-
-    nodeName = "Pointer2d"
+class BufferNode(CtrlNode):
+    """
+    Buffers the last n samples provided on input and provides them as a list of
+    length n on output.
+    A spinbox widget allows for setting the size of the buffer.
+    Default size is 32 samples.
+    """
+    nodeName = "Buffer"
+    uiTemplate = [
+        ('size',  'spin', {'value': 32.0, 'step': 1.0, 'range': [0.0, 128.0]}),
+    ]
 
     def __init__(self, name):
-        super(Pointer2d, self).__init__(name)
-        self.ir_vals = []
-        print self.btaddr
+        terminals = {
+            'dataIn': dict(io='in'),
+            'dataOut': dict(io='out'),
+        }
+        self._buffer = np.array([])
+        CtrlNode.__init__(self, name, terminals=terminals)
 
-    def initUI(self):
-        self.setWindowTitle("WiiMote 2D Pointer")
-        self.widget = QtGui.QWidget()
-        self.setCentralWidget(self.widget)
+    def process(self, **kwds):
+        size = int(self.ctrls['size'].value())
+        self._buffer = np.append(self._buffer, kwds['dataIn'])
+        self._buffer = self._buffer[-size:]
+        output = self._buffer
+        return {'dataOut': output}
+
+fclib.registerNodeType(BufferNode, [('Data',)])
+
+class WiimoteNode(Node):
+    """
+    Outputs sensor data from a Wiimote.
+
+    Supported sensors: accelerometer (3 axis)
+    Text input box allows for setting a Bluetooth MAC address.
+    Pressing the "connect" button tries connecting to the Wiimote.
+    Update rate can be changed via a spinbox widget. Setting it to "0"
+    activates callbacks everytime a new sensor value arrives (which is
+    quite often -> performance hit)
+    """
+    nodeName = "Wiimote"
+
+    def __init__(self, name):
+        terminals = {
+            'accelX': dict(io='out'),
+            'accelY': dict(io='out'),
+            'accelZ': dict(io='out'),
+        }
+        self.wiimote = None
+        self._acc_vals = []
+        self.ui = QtGui.QWidget()
         self.layout = QtGui.QGridLayout()
-        self.widget.setLayout(self.layout)
 
-    def initChart(self):
-        pass
+        label = QtGui.QLabel("Bluetooth MAC address:")
+        self.layout.addWidget(label)
+        self.text = QtGui.QLineEdit()
+        self.layout.addWidget(self.text)
+        label2 = QtGui.QLabel("Update rate (Hz)")
+        self.layout.addWidget(label2)
+        self.update_rate_input = QtGui.QSpinBox()
+        self.update_rate_input.setMinimum(0)
+        self.update_rate_input.setMaximum(60)
+        self.update_rate_input.setValue(20)
+        self.update_rate_input.valueChanged.connect(self.set_update_rate)
+        self.layout.addWidget(self.update_rate_input)
 
-fclib.registerNodeType(Pointer2d, [('Sensor',)])
+        #self.connect_button = QtGui.QPushButton("connect")
+        #self.layout.addWidget(self.connect_button)
+        self.ui.setLayout(self.layout)
+        #self.connect_button.clicked.connect(self.connect_wiimote)
+        self.btaddr = "b8:ae:6e:18:5d:ab" # for ease of use
+        self.text.setText(self.btaddr)
+        self.update_timer = QtCore.QTimer()
+        self.update_timer.timeout.connect(self.update_all_sensors)
+
+        Node.__init__(self, name, terminals=terminals)
+
+    def setBtAddr(self, btaddr):
+        self.btaddr = btaddr
+
+    def setWiiMote(self, wiimote):
+        self.wiimote = wiimote
+
+    def update_all_sensors(self):
+        if self.wiimote == None:
+            return
+        self._acc_vals = self.wiimote.accelerometer
+        # todo: other sensors...
+        self.update()
+
+    def update_accel(self, acc_vals):
+        self._acc_vals = acc_vals
+        self.update()
+
+    def ctrlWidget(self):
+        return self.ui
+
+    def connect_wiimote(self):
+        self.btaddr = str(self.text.text()).strip()
+        if self.wiimote is not None:
+            self.wiimote.disconnect()
+            self.wiimote = None
+            self.connect_button.setText("connect")
+            return
+        if len(self.btaddr) == 17 :
+            self.connect_button.setText("connecting...")
+            self.wiimote = wiimote.connect(self.btaddr)
+            if self.wiimote == None:
+                self.connect_button.setText("try again")
+            else:
+                self.connect_button.setText("disconnect")
+                self.set_update_rate(self.update_rate_input.value())
+
+    def set_update_rate(self, rate):
+        if rate == 0: # use callbacks for max. update rate
+            self.wiimote.accelerometer.register_callback(self.update_accel)
+            self.update_timer.stop()
+        else:
+            self.wiimote.accelerometer.unregister_callback(self.update_accel)
+            self.update_timer.start(1000.0/rate)
+
+    def process(self, **kwdargs):
+        x,y,z = self._acc_vals
+        return {'accelX': np.array([x]), 'accelY': np.array([y]), 'accelZ': np.array([z])}
+
+fclib.registerNodeType(WiimoteNode, [('Sensor',)])
+
+class Pointer2d(object):
+
+    def __init__(self, btaddr):
+        super(Pointer2d, self).__init__()
+        self.btaddr = "b8:ae:6e:1b:ad:a0"
+        self.wm = wiimote.connect(self.btaddr)
+
+    def initPlot(self):
+        win = QtGui.QMainWindow()
+        win.setWindowTitle('Pointer2D demo')
+        cw = QtGui.QWidget()
+        win.setCentralWidget(cw)
+        layout = QtGui.QGridLayout()
+        cw.setLayout(layout)
+
+        ## Create an empty flowchart with a single input and output
+        fc = Flowchart(terminals={
+            'dataIn': {'io': 'in'},
+            'dataOut': {'io': 'out'}
+        })
+        w = fc.widget()
+
+        layout.addWidget(fc.widget(), 0, 0, 2, 1)
+
+        pw1 = pg.PlotWidget()
+        pw2 = pg.PlotWidget()
+        layout.addWidget(pw1, 0, 1)
+        layout.addWidget(pw2, 1, 1)
+        pw1.setYRange(0, 1024)
+
+        pw2.plot([20], [50], pen=(200,200,200), symbolBrush=(255,0,0), symbolPen='w')
+
+        pw1Node = fc.createNode('PlotWidget', pos=(0, -150))
+        pw1Node.setPlot(pw1)
+
+        wiimoteNode = fc.createNode('Wiimote', pos=(0, 0), )
+        wiimoteNode.setWiiMote(self.wm)
+        bufferNode = fc.createNode('Buffer', pos=(150, 0))
+
+        fc.connectTerminals(wiimoteNode['accelX'], bufferNode['dataIn'])
+        fc.connectTerminals(bufferNode['dataOut'], pw1Node['In'])
+
+        win.show()
+        if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+            QtGui.QApplication.instance().exec_()
+
+        def getIrData(self):
+            pass
+
 
 if __name__ == '__main__':
+
     app = QtGui.QApplication([])
-    win = QtGui.QMainWindow()
-    win.setWindowTitle('Pointer2D demo')
-    cw = QtGui.QWidget()
-    win.setCentralWidget(cw)
-    layout = QtGui.QGridLayout()
-    cw.setLayout(layout)
-
-    ## Create an empty flowchart with a single input and output
-    fc = Flowchart(terminals={
-        'dataIn': {'io': 'in'},
-        'dataOut': {'io': 'out'}
-    })
-    w = fc.widget()
-
-    layout.addWidget(fc.widget(), 0, 0, 2, 1)
-
-    pw1 = pg.PlotWidget()
-    layout.addWidget(pw1, 0, 1)
-    pw1.setYRange(0, 1024)
-
-    pw1Node = fc.createNode('PlotWidget', pos=(0, -150))
-    pw1Node.setPlot(pw1)
-
-    wiimoteNode = fc.createNode('Pointer2d', pos=(0, 0), )
-    bufferNode = fc.createNode('Buffer', pos=(150, 0))
-
-    fc.connectTerminals(wiimoteNode['accelX'], bufferNode['dataIn'])
-    fc.connectTerminals(bufferNode['dataOut'], pw1Node['In'])
-
-    win.show()
+    pointer = Pointer2d("")
