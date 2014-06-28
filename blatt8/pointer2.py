d309969a054726c38c27242459a1694022f2cb4d
ManuKrapf <manuel@krapfnet.de>
2014-06-28 15:00:44 +0200

    it works more or less

--- blatt8/pointer2.py
+++ blatt8/pointer2.py
@@ -11,14 +11,14 @@ from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt4agg import NavigationToolbar2QTAgg as NavigationToolbar
 import matplotlib.pyplot as plt
 
-from matplotlib import *
+#from matplotlib import *
 
 
 class Template(object):
 
     def __init__(self, name, points=[]):
         super(Template, self).__init__()
-        self.name = ""
+        self.name = name
         self.points = points
 
     def setPoints(self, points):
@@ -27,28 +27,90 @@ class Template(object):
 
 class GestureRecognition(object):
 
+    """
+    Class for the recognition of gestures with the one
+    dollar gesture recognizer.
+    The class can recognize a gesture of a given set of
+    points or store a new template with a set of points
+    Sources for the architecture and functionality of the
+    class:
+    http://www.cs.columbia.edu/~coms6998-11/papers/
+    dollargestures.pdf
+    https://github.com/voidplus/onedollar-unistroke-
+    recognizer/blob/master/src/de/voidplus/dollar/Recognizer.java
+    """
+
     def __init__(self, size=100):
         super(GestureRecognition, self).__init__()
         self.templates = []
         self.size = size
+        self.initTemplates()
 
+    def initTemplates(self):
+        # Square Template
+        points = [[0, 0], [1, 0], [2, 0], [3, 0], [4, 0],
+                  [5, 0], [6, 0], [7, 0], [8, 0], [9, 0],
+                  [10, 0], [11, 0], [12, 0], [13, 0], [14, 0],
+                  [15, 0], [15, 1], [15, 2], [15, 3], [15, 4],
+                  [15, 5], [15, 6], [15, 7], [15, 8], [15, 9],
+                  [15, 10], [15, 11], [15, 12], [15, 13], [15, 14],
+                  [15, 15], [14, 15], [13, 15], [12, 15], [11, 15],
+                  [10, 15], [9, 15], [8, 15], [7, 15], [6, 15],
+                  [5, 15], [4, 15], [3, 15], [2, 15], [1, 15],
+                  [0, 15], [0, 14], [0, 13], [0, 12], [0, 11],
+                  [0, 10], [0, 9], [0, 8], [0, 7], [0, 6],
+                  [0, 5], [0, 4], [0, 3], [0, 2], [0, 1],
+                  ]
+        newPoints = self.processRawData(points)
+        temp = Template("Square", newPoints)
+        self.templates.append(temp)
+        # Circle Template
+        points = []
+        for i in range(64):
+            a = i*5.625
+            x = math.cos(a)
+            y = math.sin(a)
+            points.append([x, y])
+        newPoints = self.processRawData(points)
+        temp = Template("Circle", newPoints)
+        self.templates.append(temp)
+        # Triangle Template
+        points = [[0, 0], [10, 0], [20, 0], [10, 10]]
+        newPoints = self.processRawData(points)
+        temp = Template("Triangle", newPoints)
+        self.templates.append(temp)
+
+    """
+    Recognizing a template
+    """
     def recognizeGesture(self, points):
         newPoints = self.processRawData(points)
         temp = self.recognize(newPoints, self.templates)
         return temp
 
+    """
+    Storing a new template
+    """
     def storeTemplate(self, name, points):
         newPoints = self.processRawData(points)
         temp = Template(name, newPoints)
         self.templates.append(temp)
 
+    """
+    Processes the given raw data points to recognize a
+    gesture or storing a new template
+    """
     def processRawData(self, points):
-        newPoints = self.resample(points)
-        newPoints = self.rotateToZero(newPoints)
-        newPoints = self.scaleToSquare(newPoints)
-        newPoints = self.translateToOrigin(newPoints)
-        return newPoints
+        if(len(points) > 0):
+            newPoints = self.resample(points)
+            newPoints = self.rotateToZero(newPoints)
+            newPoints = self.scaleToSquare(newPoints)
+            newPoints = self.translateToOrigin(newPoints)
+            return newPoints
+        else:
+            return 0
 
+    """Step 1 from the 1 Dollar Gesture Recognizer"""
     def resample(self, points, step=64):
         newPoints = []
         length = self.total_length(points)
@@ -62,7 +124,7 @@ class GestureRecognition(object):
             if curpos+d >= stepsize:
                 nx = p1[0] + ((stepsize-curpos)/d)*(points[i][0]-p1[0])
                 ny = p1[1] + ((stepsize-curpos)/d)*(points[i][1]-p1[1])
-                newpoints.append([nx, ny])
+                newPoints.append([nx, ny])
                 points.insert(i, [nx, ny])
                 curpos = 0
             else:
@@ -83,6 +145,7 @@ class GestureRecognition(object):
             p1 = points[i]
         return length
 
+    """Step 2 from the 1 Dollar Gesture Recognizer"""
     def rotateToZero(self, points):
         c = self.getCentroId(points)
         theta = math.atan2((c[1]-points[0][1]), (c[0]-points[0][0]))
@@ -106,6 +169,7 @@ class GestureRecognition(object):
         cId[1] /= len(points)
         return cId
 
+    """Step 3 from the 1 Dollar Gesture Recognizer"""
     def scaleToSquare(self, points):
         newPoints = []
         b = self.getBoundingBox(points)
@@ -116,8 +180,8 @@ class GestureRecognition(object):
         return newPoints
 
     def getBoundingBox(self, points):
-        minX, minY = float("inf")
-        maxX, maxY = float("-inf")
+        minX = minY = float("inf")
+        maxX = maxY = float("-inf")
         for p in points:
             minX = min(p[0], minX)
             maxX = max(p[0], maxX)
@@ -134,6 +198,7 @@ class GestureRecognition(object):
             newPoints.append([qx, qy])
         return newPoints
 
+    """Step 4 from the 1 Dollar Gesture Recognizer"""
     def recognize(self, points, templates):
         b = float("inf")
         trec = None
@@ -171,11 +236,14 @@ class GestureRecognition(object):
         d = self.pathDistance(newPoints, temp.points)
         return d
 
-    def pathDistance(a, b):
+    def pathDistance(self, a, b):
         d = 0
-        for i in range(0, len(a)):
-            d += self.distance(a[i], b[i])
-        return d/len(a)
+        if(len(a) != len(b)):
+            return float("inf")
+        else:
+            for i in range(0, len(a)):
+                d += self.distance(a[i], b[i])
+            return d/len(a)
 
 
 class Pointer(QtGui.QDialog):
@@ -183,87 +251,93 @@ class Pointer(QtGui.QDialog):
     def __init__(self, parent=None):
         super(Pointer, self).__init__(parent)
         self.btaddr = "b8:ae:6e:1b:ad:a0"
-        #self.wm = wiimote.connect(self.btaddr)
+        self.wm = wiimote.connect(self.btaddr)
+        self.wm.buttons.register_callback(self.button_click)
         self.gr = GestureRecognition()
-        self.buffer = []
+        #self.buffer = []
+        self.curPos = []
         self.ir_vals = []
+        self.recPoints = False
         self.step = 64
         self.t = QtCore.QTimer()
         self.t.timeout.connect(self.updateData)
+        self.t.start(50)
         self.initPlot()
 
     def initPlot(self):
         # a figure instance to plot on
         self.figure = plt.figure()
-
         # this is the Canvas Widget that displays the `figure`
         # it takes the `figure` instance as a parameter to __init__
         self.canvas = FigureCanvas(self.figure)
-
         # this is the Navigation widget
         # it takes the Canvas widget and a parent
         self.toolbar = NavigationToolbar(self.canvas, self)
-
-        # Just some button connected to `plot` method
-        #self.button = QtGui.QPushButton('Plot')
-        #self.button.clicked.connect(self.plot)
-
         # set the layout
         layout = QtGui.QVBoxLayout()
+        self.status = QtGui.QLabel()
+        self.status.setText("Press A for Recognition or B for Recording!")
+        self.label = QtGui.QLabel()
+        self.label.setText("No Gesture")
         layout.addWidget(self.toolbar)
+        layout.addWidget(self.status)
+        layout.addWidget(self.label)
         layout.addWidget(self.canvas)
-        #layout.addWidget(self.button)
         self.setLayout(layout)
 
     def updateData(self):
-        #self.getIrData()
-        #avg = getAverage()
-        self.ir_vals = [[0,1],[1,0],[2,-1],[3,0],[4,1],[5,0],[6,-1],[7,0],[8,1],[9,0],[10,-1],[11,0],[12,1],[13,0],[14,-1]]
-        self.plotIt()
+        self.getIrData()
+        self.plot()
 
-    def plotIt(self):
-        np = self.gr.resample(self.ir_vals, self.step)
-        #ylim(-1.5, 1.5)
-        #plot(*zip(*np), ls="solid", marker='o', color='r')
-        self.plot(np)
-
-    def plot(self, data):
+    def plot(self):
         # create an axis
         ax = self.figure.add_subplot(111)
-
+        # set axis limits
+        ax.axis([0, 1000, 0, 1000])
+        ax.autoscale(enable=False)
         # discards the old graph
         ax.hold(False)
-
-        # plot data
-        ax.plot(*zip(*data), ls="solid", marker='o', color='r')
-
+        if(self.recPoints):
+            data = self.ir_vals
+            ax.plot(*zip(*data), ls="solid", marker='o', color='r')
+        else:
+            data = self.curPos
+            ax.plot([data[0]], [data[1]], ls="solid", marker='o', color='r')
         # refresh canvas
         self.canvas.draw()
 
     def getIrData(self):
         irobj = self.wm.ir
-        self.getBiggestVal(irobj)
+        if(len(irobj) > 0):
+            self.getBiggestVal(irobj)
+        else:
+            self.curPos = [0, 0]
 
     def getBiggestVal(self, curr_ir):
         biggest = [0, 0, -1]
         for ir_obj in curr_ir:
             if(biggest[2] < ir_obj["size"]):
-                biggest = [ir_obj["x"], ir_obj["y"]]
+                biggest = [ir_obj["x"], ir_obj["y"],
+                           ir_obj['size']]
         if(len(self.ir_vals) >= self.step):
             del self.ir_vals[0]
-        self.ir_vals.append(biggest)
+        self.curPos = [biggest[0], biggest[1]]
+        if(self.recPoints):
+            self.ir_vals.append(self.curPos)
+            self.ir_vals = self.ir_vals[-self.step:]
 
     def getAverage(self):
         sumX = 0
         sumY = 0
-        sumSize = 0
-        for val in self.ir_vals:
-            sumX += val[0]
-            sumY += val[1]
-            sumSize += val[2]
-        return [np.array([(sumX/len(self.ir_vals))]),
-                np.array([(sumY/len(self.ir_vals))]),
-                int(sumSize/len(self.ir_vals))]
+        if(len(self.ir_vals) > 0):
+            for val in self.ir_vals:
+                sumX += val[0]
+                sumY += val[1]
+            return [np.array([(sumX/len(self.ir_vals))]),
+                    np.array([(sumY/len(self.ir_vals))]),
+                    ]
+        else:
+            return [0, 0]
 
     def button_click(self, button):
         if(len(button) >= 1):
@@ -279,20 +353,24 @@ class Pointer(QtGui.QDialog):
                     self.stopRecording()
 
     def startRecognition(self):
+        self.status.setText("start Recognition")
         self.ir_vals = []
-        self.t.start(50)
+        self.recPoints = True
 
     def stopRecognition(self):
-        self.t.stop()
+        self.status.setText("stop Recognition")
+        self.recPoints = False
         temp = self.gr.recognizeGesture(self.ir_vals)
-        print(temp)
+        self.label.setText(temp['template'].name)
 
     def startRecording(self):
+        self.status.setText("start Recording")
         self.ir_vals = []
-        self.t.start(50)
+        self.recPoints = True
 
     def stopRecording(self):
-        self.t.stop()
+        self.status.setText("stop Recording")
+        self.recPoints = False
         self.gr.storeTemplate("test", self.ir_vals)
 
 
